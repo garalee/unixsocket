d7285d28f097d9367874163dd9244fa48eae510d
Garam <admin@localhost.localdomain>
2014-09-25 00:23:27 -0400

    Echo Server and Client has been completed

--- server.c
+++ server.c
@@ -5,9 +5,13 @@
 #include<sys/socket.h>
 #include<unistd.h>
 #include<stdlib.h>
+
 #define SERVER_PORT 9999
 
+#define MAXLINE 1024
+
 void init_server_addr(struct sockaddr_in * sa);
+void str_echo(int sockfd);
 
 int main(void){
     int serverfd,clientfd;
@@ -38,10 +42,25 @@ int main(void){
     return 0;
 }
 
-
 void init_server_addr(struct sockaddr_in * sa){
     bzero(sa,sizeof(struct sockaddr_in));
     sa->sin_family = AF_INET;
     sa->sin_addr.s_addr = htonl(INADDR_ANY);
     sa->sin_port = htons(SERVER_PORT);
 }
+
+void str_echo(int sockfd){
+    ssize_t n;
+    char buf[MAXLINE];
+
+again:
+    while ( (n = read(sockfd,buf,MAXLINE)) > 0){
+	printf("SERVER RECVED: %s\n",buf);
+	write(sockfd,buf,n);
+    }
+
+    if( n < 0)
+	printf("str_echo: read error\n");
+}
+
+	
